ba9431ac92c8dc8de420e86004de78dfedc760ec
Lillemor Eriksson <lillemor_eira@hotmail.com>
2018-02-02 16:18:23 +0100

    0202

--- Lektion 0129 MVC/Controllers/HomeController.cs
+++ Lektion 0129 MVC/Controllers/HomeController.cs
@@ -9,13 +9,28 @@ namespace Lektion_0129_MVC.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Abuot()
+        public ActionResult About()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult TestingForm()
+        {
+            return View();
+        }
+
+        [ HttpPost]
+        public ActionResult TestingForm(int multi)
+        {
+            ViewBag.multi = multi;
+
             return View();
         }
     }
